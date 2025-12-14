4964e086b93460fa968bb759a6b34440eb446e1b
catherineli.js <catherineli808@gmail.com>
2025-12-13 17:28:54 -0800

    Uninstall packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,5 @@
-import Select from "react-select";
-
-const options = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla", isClearable: true },
-];
-
 function App() {
-  return (
-    <>
-      <Select isClearable={true} options={options} />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
